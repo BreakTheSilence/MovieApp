f1f390225334c573b696571def2beb0c3809d99c
Kirill Juferev <kirill.juferev@bait.ee>
2024-03-15 15:21:52 +0200

    Initial Commit

---
+++ MovieAppWpf/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MovieAppWpf;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
